deccceed2796026c981fa70ca85287f00621cc53
Nicolás García <115923200+naiitch@users.noreply.github.com>
2026-01-09 13:19:24 +0100

    Delete back directory

--- back/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SECRET_KEY = "super_secret_key"
-    DB_HOST = "localhost"
-    DB_USER = "user"
-    DB_PASSWORD = "password"
-    DB_NAME = "finance_app"
--- back/utils/auth.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_password(password):
-    return generate_password_hash(password)
-
-def check_password(password, hash_):
-    return check_password_hash(hash_, password)
